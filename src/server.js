098201e90ece041b3f55cec26769277f379f8b1d
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-06-12 15:51:29 +0530

    resolve file path

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
+//Resolving file path
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//Resolving env file path
 import dotenv from "dotenv";
-dotenv.config();
-import app from "./app.js";
-const PORT = process.env.PORT || 5000;
+dotenv.config({path:`${__dirname}/../.env`});
 
 import connectDB from "./config/db.js";
 import { getRedisClient } from "./config/redis.js";
 import { getTransporter } from "./config/mail.js";
+import app from "./app.js";
+
+const PORT = process.env.PORT || 5000;
+
 const startServer = async () => {
   try {
+
     // --- Initialize infrastructure here ---
     await connectDB();
     await getRedisClient();
@@ -16,6 +26,7 @@ const startServer = async () => {
     app.listen(PORT, () => {
       console.log(process.env.PORT)
       console.log(`Server running on port ${PORT}`);
+      console.log(`Current working dir ${process.cwd()}`);
     });
 
   } catch (error) {
